f3274f583bff5a74842fe8d7ab3b4c0052049f81
Roberto Arciniegas <robertoarciniegas@robertos-macbook-air.local>
2015-12-14 00:44:54 -0500

    Lesson 3a finished

--- PlaySoundsViewController.swift
+++ PlaySoundsViewController.swift
@@ -7,7 +7,44 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
+    
+    var audioPlayer:AVAudioPlayer = AVAudioPlayer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var filePathURL:NSURL = NSBundle.mainBundle().URLForResource("movie_quote", withExtension: "mp3")!
+        do { audioPlayer = try AVAudioPlayer(contentsOfURL: filePathURL, fileTypeHint: nil) } catch _ { return print("file not found") }
+        audioPlayer.enableRate = true
+        
+    }
 
+    @IBAction func playSlowAudio(sender: UIButton) {
+        
+        playAudioWithVariableRate(0.5)
+        
+    }
+    @IBAction func playFastAudio(sender: UIButton) {
+        
+        playAudioWithVariableRate(1.5)
+        
+    }
+
+
+    @IBAction func stopAudio(sender: UIButton) {
+        
+        audioPlayer.stop()
+        
+    }
+    
+    func playAudioWithVariableRate(rate: float_t){
+        
+        audioPlayer.prepareToPlay()
+        audioPlayer.stop()
+        audioPlayer.rate = rate
+        audioPlayer.play()
+        
+    }
 }
